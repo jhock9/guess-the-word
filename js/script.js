8b7ed5d73b5017f8132f6d4a36a7e04fd5bdacfe
jhock9 <jon.hocker@gmail.com>
2021-06-23 14:53:54 -0500

    accepting and validating player guesses

--- js/script.js
+++ js/script.js
@@ -15,10 +15,13 @@ const message = document.querySelector(".message");
 //The hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again");
 //Starting word to test hte game until I fetch words from API
-const word = "magnolia";
+
+const word = "magnolia"; // test word
+const guessedLetters = [];
+
 
 // Display symbols as placeholders for the chosen word's letters
-const placeholder = function (/*word*/) {
+const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
     console.log(letter);
@@ -29,9 +32,58 @@ const placeholder = function (/*word*/) {
 
 placeholder(word);
 
-guessButton.addEventListener("click", function (e) {
-  e.preventDefault(); //prevents the default behavior of clicking a button, the form submitting, and then reloading the page
+/*
+guessLetterButton.addEventListener("click", function (e) {
+  e.preventDefault();
   const guess = letterInput.value;
   console.log(guess);
   letterInput.value = "";
 });
+*/
+
+guessButton.addEventListener("click", function (e) {
+  // Prevents the default behavior of clicking a button, the form submitting, and then reloading the page
+  e.preventDefault(); 
+  // Empty message paragraph
+  message.innerText = "";
+  // Let's grab what was entered in the input
+  const guess = letterInput.value;
+  // Let's make sure that it is a single letter
+  const goodGuess = validateInput(guess);
+
+  if (goodGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(guess);
+  };
+  // Empty guess text input
+  letterInput.value = "";
+});
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/
+  if (input.length === 0) {
+    // Is the input empty?
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    // Did you type more than one letter?
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    // Did you type a number, a special character or some other non letter thing?
+    message.innerText = "Please enter a letter from A-Z.";
+  } else {
+    // We finally got a single letter, omg yay
+  return input;
+  }
+};
+
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
+
+
